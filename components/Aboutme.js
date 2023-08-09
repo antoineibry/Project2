ac909397bc422ea06fed98818cf76a13a7840583
antoineibry <antoine.ibry@outlook.com>
2023-08-09 11:50:53 +0300

    example

--- components/Aboutme.js
+++ components/Aboutme.js
@@ -4,7 +4,8 @@ import Image from 'next/image'
 
 const Aboutme = () => {
   return (
-    <section className="bg-gray-200 grid grid-flow-col grid-cols-2 justify-center px-2 py-14">
+    <section className=" bg-gray-200">
+            <div className="2xl:container mx-auto grid grid-flow-col grid-cols-2 justify-center px-2 py-14">
             <div className="flex items-center justify-center">
               <Image src="/images/antoine-teaching.png" alt="Antoine teaching" className="rounded-3xl" width={300} height={200}/>
             </div>
@@ -14,6 +15,7 @@ const Aboutme = () => {
                 <p className="text-xl mt-7 font-bold">Antoine Ibry</p>
                 <p className="text-lg font-semibold mb-7">Private Tutor</p>
                 <a className="p-3 -ml-3 bg-myblue text-white rounded-3xl font-semibold border-none" href="#"><button>Contact Antoine</button></a>
+            </div>
             </div>
     </section>
   )
