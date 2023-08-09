a669f56048cc0051843cbc0e752e9d9f60bb313b
antoineibry <antoine.ibry@outlook.com>
2023-08-09 13:43:32 +0300

    new pics

--- components/TextSection.js
+++ components/TextSection.js
@@ -5,12 +5,12 @@ import Image from 'next/image'
 const TextSection = ({SECTION_IMAGE, TEXT1, TEXT2, TEXT3, BUTTON}) => {
   return (
     <section className=" bg-gray-200">
-            <div className="2xl:container mx-auto grid grid-flow-col grid-cols-2 justify-center px-2 py-14">
+            <div className="2xl:container mx-auto grid grid-flow-col grid-cols-3 justify-center px-32 py-14">
             <div className="flex items-center justify-center">
-              <Image src={SECTION_IMAGE} alt="Antoine teaching" className="rounded-3xl" width={300} height={200}/>
+              <Image src={SECTION_IMAGE} alt="Antoine teaching" className="rounded-3xl" width={350} height={200}/>
             </div>
 
-            <div className="text-left mt-14 font-medium text-black text-lg pl-6">
+            <div className="text-left mt-14 font-medium text-black text-lg pl-6 col-span-2">
                 <p><i>{TEXT1}</i></p>
                 <p className="text-xl mt-7 font-bold">{TEXT2}</p>
                 <p className="text-lg font-semibold mb-7">{TEXT3}</p>
