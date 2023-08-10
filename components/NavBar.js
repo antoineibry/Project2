ff3de7ed5e7e7105885dab77bafa828fed231ed1
antoineibry <antoine.ibry@outlook.com>
2023-08-10 11:25:57 +0300

    mm

--- components/NavBar.js
+++ components/NavBar.js
@@ -1,7 +1,13 @@
+'use client'
+
 import Image from "next/image";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 const NavBar = () => {
+
+  const currentRoute = usePathname();
+
   return (
     <nav className="py-3 px-10 bg-myblue font-medium text-white decoration-0">
     <div className="2xl:container mx-auto flex justify-between items-center ">
@@ -14,19 +20,47 @@ const NavBar = () => {
        
        />
     </Link>
-    
+
     <ul className="list-none">
         <li className="hover:underline inline-block py-0 px-5 min-w-fit">
-            <Link href="/services" className="nav-link">Services</Link>
+            <Link href="/services" passHref>
+              <div className={
+                currentRoute === "/services"?
+                " underline" : " no-underline"
+              }>
+                Services
+              </div>
+            </Link>
         </li>
         <li className="hover:underline inline-block py-0 px-5 min-w-fit">
-            <Link href="/about" className="nav-link">About</Link>
+            <Link href="/about" passHref>
+            <div className={
+                currentRoute === "/about"?
+                " underline" : " no-underline"
+              }>
+                About
+              </div>
+            </Link>
         </li>
         <li className="hover:underline inline-block py-0 px-5 min-w-fit">
-            <Link href="/booking" className="nav-link">Booking</Link>
+            <Link href="/booking" passHref>
+            <div className={
+                currentRoute === "/booking"?
+                " underline" : " no-underline"
+              }>
+                Booking
+              </div>
+            </Link>
         </li>
         <li className="hover:underline inline-block py-0 px-5 min-w-fit">
-            <Link href="/pricing" className="nav-link">Pricing</Link>
+            <Link href="/pricing" passHref>
+            <div className={
+                currentRoute === "/pricing"?
+                " underline" : " no-underline"
+              }>
+                Pricing
+              </div>
+            </Link>
         </li>
     </ul>
     </div>
